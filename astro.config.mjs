e7f066f28d82e3d2e5981e9434195a79396d6d17
Joona Tiinanen <joona.tiinanen@basemark.com>
2023-09-08 16:30:39 +0300

    Deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://DrJonki.github.io",
+  base: "/starlight-test",
   integrations: [
     starlight({
       title: "Hypothetical Studio App Documentation",
